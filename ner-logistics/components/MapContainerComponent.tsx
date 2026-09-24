4e311e48a8a47df856a2dc2b2858dd9a7bde8855
Shaivy Ahluwalia <shaivyahluwalia@Shaivys-MacBook-Air.local>
2026-09-25 01:23:45 +0530

    realtime map

--- ner-logistics/components/MapContainerComponent.tsx
+++ ner-logistics/components/MapContainerComponent.tsx
@@ -206,7 +206,7 @@ export default function MapContainerComponent() {
       >
         {/* Dark tile layer */}
         <TileLayer
-          url={`https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?api_key=${process.env.NEXT_PUBLIC_CARTO_API_KEY}`}
+          url={`https://basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png?key=${process.env.NEXT_PUBLIC_CARTO_API_KEY}`}
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
 
